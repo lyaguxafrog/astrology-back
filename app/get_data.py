cfca5c55e8ffa09919c08afc668d5483be597d6e
adrian <maccree.a@yandex.ru>
2023-08-18 03:01:50 +0300

    тесты

--- app/get_data.py
+++ app/get_data.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from geopy.geocoders import Nominatim
 import ephem
 
@@ -15,7 +15,9 @@ def index():
         location = geolocator.geocode(location_name)
         if location is None:
             error_message = "Место не найдено. Пожалуйста, введите допустимое местоположение."
-            return render_template('index.html', error_message=error_message)
+            if request.accept_mimetypes.best == 'application/json':
+                return jsonify({'error': error_message}), 400
+            return render_template('input.html', error_message=error_message)
 
         obs = ephem.Observer()
         obs.lat, obs.lon = str(location.latitude), str(location.longitude)
@@ -30,81 +32,27 @@ def index():
 
         house_degrees_placidus = []
         house_degrees_koch = []
-        house_degrees_porphyry = []
-        house_degrees_regiomontanus = []
-        house_degrees_campanus = []
-        house_degrees_equal = []
-        house_degrees_whole_sign = []
 
         for house_num in range(1, 13):
             house_cusp = obs.sidereal_time() - (30 * (house_num - 1)) * ephem.degree
-
-
-
-            # Placidus
+            planet = ephem.Sun(obs)
             planet.compute(obs)
+            cusp_longitude = house_cusp * 180.0 / ephem.pi
             planet_longitude = planet.a_ra * 180.0 / ephem.pi
-            separation = planet_longitude - (house_cusp * 180.0 / ephem.pi)
+            separation = planet_longitude - cusp_longitude
             if separation < 0:
                 separation += 360.0
             house_degrees_placidus.append(separation)
 
-            # Koch
             planet.compute(obs)
             planet_longitude = planet.a_ra * 180.0 / ephem.pi
-            separation = planet_longitude - (house_cusp * 180.0 / ephem.pi)
+            separation = planet_longitude - cusp_longitude
             if separation < 0:
                 separation += 360.0
             house_degrees_koch.append(separation)
 
-            # Porphyry
-            planet.compute(obs)
-            planet_longitude = planet.a_ra * 180.0 / ephem.pi
-            separation = planet_longitude - (house_cusp * 180.0 / ephem.pi)
-            if separation < 0:
-                separation += 360.0
-            house_degrees_porphyry.append(separation)
-
-            # Regiomontanus
-            planet.compute(obs)
-            planet_longitude = planet.a_ra * 180.0 / ephem.pi
-            separation = planet_longitude - (house_cusp * 180.0 / ephem.pi)
-            if separation < 0:
-                separation += 360.0
-            house_degrees_regiomontanus.append(separation)
-
-            # Campanus
-            planet.compute(obs)
-            planet_longitude = planet.a_ra * 180.0 / ephem.pi
-            separation = planet_longitude - (house_cusp * 180.0 / ephem.pi)
-            if separation < 0:
-                separation += 360.0
-            house_degrees_campanus.append(separation)
-
-            # Equal
-            planet.compute(obs)
-            planet_longitude = planet.a_ra * 180.0 / ephem.pi
-            separation = planet_longitude - (house_cusp * 180.0 / ephem.pi)
-            if separation < 0:
-                separation += 360.0
-            house_degrees_equal.append(separation)
-
-            # Whole Sign
-            planet.compute(obs)
-            planet_longitude = planet.a_ra * 180.0 / ephem.pi
-            separation = planet_longitude - (house_cusp * 180.0 / ephem.pi)
-            if separation < 0:
-                separation += 360.0
-            house_degrees_whole_sign.append(separation)
-
         return render_template('result.html', planet_degrees=planet_degrees,
                                house_degrees_placidus=house_degrees_placidus,
-                               house_degrees_koch=house_degrees_koch,
-                               house_degrees_porphyry=house_degrees_porphyry,
-                               house_degrees_regiomontanus=house_degrees_regiomontanus,
-                               house_degrees_campanus=house_degrees_campanus,
-                               house_degrees_equal=house_degrees_equal,
-                               house_degrees_whole_sign=house_degrees_whole_sign)
+                               house_degrees_koch=house_degrees_koch)
     
     return render_template('input.html')
-
