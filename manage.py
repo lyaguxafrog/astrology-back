fc912573f58424d431c662d58513eb2f900119b3
adrian <maccree.a@yandex.ru>
2023-08-18 03:05:14 +0300

    тесты

--- manage.py
+++ manage.py
@@ -1,3 +1,15 @@
 #!flask/bin/python
+import sys
 from app import app
-app.run(debug = True)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == 'tests':
+        import unittest
+        tests = unittest.TestLoader().discover('tests')
+        result = unittest.TextTestRunner(verbosity=2).run(tests)
+        if result.wasSuccessful():
+            sys.exit(0)
+        else:
+            sys.exit(1)
+    else:
+        app.run(debug=True)
