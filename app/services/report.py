d5e225282143d07d378cce30a23cf6bf3789448f
adrian <maccree.a@yandex.ru>
2023-08-20 18:59:58 +0300

    тесты

--- app/services/report.py
+++ app/services/report.py
@@ -5,9 +5,7 @@ def astr_report(name: str, year: int, month: int, day: int, hour: int, minute: i
     human = AstrologicalSubject(name, year, month, day, hour, minute, city, nation='RU')
     report = Report(human)
     
-    report_text = f"+- Kerykeion report for {name} -+\n"
-    report_text += str(report.get_full_report())  # Преобразование объекта отчета в строку
+
+    report_text = str(report.get_full_report())  # Преобразование объекта отчета в строку
     return report_text
 
-
-# print(astr_report('Адриан', 2010,  10, 10, 10, 10, 'Москва'))
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,36 +1,74 @@
 import unittest
-from app import app
+from app.services.report import astr_report
 
-class AstrologyAppTestCase(unittest.TestCase):
+class TestAstrologicalReport(unittest.TestCase):
 
-    def setUp(self):
-        self.client = app.test_client()
 
-    def test_index_page(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('Введите данные для расчета', response.data.decode())
 
-    def test_valid_form_submission(self):
-        response = self.client.post('/', data={
-            'date': '2023-08-18',
-            'time': '12:00',
-            'location': 'New York, USA'
-        })
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('Результаты', response.data.decode())
-        self.assertIn('Градусы планет', response.data.decode())
-        self.assertIn('Градусы домов (Placidus)', response.data.decode())
-        # Добавьте проверки для других систем домов
+    def test_report_output(self):
+        self.maxDiff = None
+        expected_output = (
+            "+- Kerykeion report for Адриан -+\n"
+            "+------------+-------+------------+-----------+----------+\n"
+            "| Date       | Time  | Location   | Longitude | Latitude |\n"
+            "+------------+-------+------------+-----------+----------+\n"
+            "| 10/10/2010 | 10:10 | Москва, RU | 37.61556  | 55.75222 |\n"
+            "+------------+-------+------------+-----------+----------+\n"
+            "+-----------+------+-------+------+----------------+\n"
+            "| Planet    | Sign | Pos.  | Ret. | House          |\n"
+            "+-----------+------+-------+------+----------------+\n"
+            "| Sun       | Lib  | 16.84 | -    | Eleventh_House |\n"
+            "| Moon      | Sco  | 20.67 | -    | First_House    |\n"
+            "| Mercury   | Lib  | 11.81 | -    | Eleventh_House |\n"
+            "| Venus     | Sco  | 13.16 | R    | First_House    |\n"
+            "| Mars      | Sco  | 17.26 | -    | First_House    |\n"
+            "| Jupiter   | Pis  | 25.99 | R    | Fourth_House   |\n"
+            "| Saturn    | Lib  | 8.88  | -    | Eleventh_House |\n"
+            "| Uranus    | Pis  | 27.88 | R    | Fourth_House   |\n"
+            "| Neptune   | Aqu  | 26.13 | R    | Third_House    |\n"
+            "| Pluto     | Cap  | 2.97  | -    | Second_House   |\n"
+            "| Mean_Node | Cap  | 6.69  | R    | Second_House   |\n"
+            "| True_Node | Cap  | 6.15  | R    | Second_House   |\n"
+            "| Chiron    | Aqu  | 26.42 | R    | Third_House    |\n"
+            "+-----------+------+-------+------+----------------+\n"
+            "+----------------+------+----------+\n"
+            "| House          | Sign | Position |\n"
+            "+----------------+------+----------+\n"
+            "| First_House    | Sco  | 8.96     |\n"
+            "| Second_House   | Sag  | 7.12     |\n"
+            "| Third_House    | Cap  | 15.02    |\n"
+            "| Fourth_House   | Aqu  | 26.64    |\n"
+            "| Fifth_House    | Pis  | 28.98    |\n"
+            "| Sixth_House    | Ari  | 22.0     |\n"
+            "| Seventh_House  | Tau  | 8.96     |\n"
+            "| Eighth_House   | Gem  | 7.12     |\n"
+            "| Ninth_House    | Can  | 15.02    |\n"
+            "| Tenth_House    | Leo  | 26.64    |\n"
+            "| Eleventh_House | Vir  | 28.98    |\n"
+            "| Twelfth_House  | Lib  | 22.0     |\n"
+            "+----------------+------+----------+\n"
+        )
+        report_output = astr_report('Адриан', 2010, 10, 10, 10, 10, 'Москва')
+        
+        # Strip whitespace and replace multiple spaces with single space
+        expected_output_cleaned = expected_output.strip().replace(' +', '+')
+        report_output_cleaned = report_output.strip().replace(' +', '+')
+        
+        self.assertEqual(report_output_cleaned, expected_output_cleaned)
 
-    def test_invalid_location_submission(self):
-        response = self.client.post('/', data={
-            'date': '2023-08-18',
-            'time': '12:00',
-            'location': 'Nonexistent Location'
-        })
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('Место не найдено', response.data.decode())
+
+    def test_report_content(self):
+        self.maxDiff = None
+        report_output = astr_report('Адриан', 2010, 10, 10, 10, 10, 'Москва')
+        self.assertIn("+- Kerykeion report for Адриан -+", report_output)
+        self.assertIn("Sun       | Lib  | 16.84 | -    | Eleventh_House", report_output)
+        self.assertIn("Neptune   | Aqu  | 26.13 | R    | Third_House", report_output)
+        self.assertIn("Twelfth_House  | Lib  | 22.0     |", report_output)
+
+    def test_invalid_input(self):
+        self.maxDiff = None
+        with self.assertRaises(ValueError):
+            astr_report('Иван', 2023, 13, 45, 8, 30, 'Москва')
 
 if __name__ == '__main__':
     unittest.main()
