ad7ca228c4f71be8b3f0071a3210e98040c0b5e4
adrian <maccree.a@yandex.ru>
2023-08-23 02:59:30 +0300

    калькулятор планет

--- app/services/planet.py
+++ app/services/planet.py
@@ -4,78 +4,103 @@ import swisseph as swe
 import os
 from dotenv import load_dotenv, find_dotenv
 
+from time_convert import get_julian_datetime
+
 # Загрузка переменных окружения из файла .env
 load_dotenv(find_dotenv())
 
 # Установка пути к эфемеридам
 swe.set_ephe_path(os.getenv('EPH_PATH'))
 
-def calculate_planet_position(hours: int, day: int, month: int, year: int, planet_id):
-    """
-    Получение данных о планетах по времени
 
-    :params hour: Час
-    :params day: День
-    :params month: Месяц
-    :params year: Год
+class Planets:
+    def __init__(self, year: int, month: int, day: int, hour: int, minute: int) -> None:
+        """
+        Конструктор класса Planets. Принимает параметры для инициализации объекта.
 
-    :returns: Список всех планет
-    """
+        :params year: Год
+        :params month: Месяц
+        :params day: День
+        :params hour: Час
+        :params minute: Минута
+        """
+        self.year = year
+        self.month = month
+        self.day = day
+        self.hour = hour
+        self.minute = minute
+        
+    # def start_end_position(self, planet_id: int):
 
-    planet_names = {
-    swe.SUN: "Sun",
-    swe.MOON: "Moon",
-    swe.MERCURY: "Mercury",
-    swe.VENUS: "Venus",
-    swe.MARS: "Mars",
-    swe.JUPITER: "Jupiter",
-    swe.SATURN: "Saturn",
-    swe.URANUS: "Uranus",
-    swe.NEPTUNE: "Neptune",
-    swe.PLUTO: "Pluto",
-    swe.MEAN_NODE: "Mean Node",
-    swe.TRUE_NODE: "True Node"
-}
+    #     # Преобразования в Юлианский календарь
+    #     start_jd_date = get_julian_datetime(year=self.year, month=self.month, day=self.day, hour=self.hour, minute=self.minute)
+    #     end_jd_date = get_julian_datetime(year=self.year, month=self.month, day=self.day + 1, hour=self.hour, minute=self.minute)
 
-    # Вычисляем юлианскую дату в терминах эфемеридного времени (ET)
-    jd_start = swe.julday(year, month, day, hours)
-    jd_end = swe.julday(year, month, day + 1, hours)  # Конец дня
 
-    # Рассчитываем позицию планеты на начало и конец дня
-    start_planet_position, _ = swe.calc_ut(jd_start, planet_id)  # Игнорируем второй элемент
-    end_planet_position, _ = swe.calc_ut(jd_end, planet_id)  # Игнорируем второй элемент
+    #     planet_names = {
+    #         swe.SUN: "Sun",
+    #         swe.MOON: "Moon",
+    #         swe.MERCURY: "Mercury",
+    #         swe.VENUS: "Venus",
+    #         swe.MARS: "Mars",
+    #         swe.JUPITER: "Jupiter",
+    #         swe.SATURN: "Saturn",
+    #         swe.URANUS: "Uranus",
+    #         swe.NEPTUNE: "Neptune",
+    #         swe.PLUTO: "Pluto",
+    #         swe.MEAN_NODE: "Mean Node",
+    #         swe.TRUE_NODE: "True Node"
+    # }
 
-    # Рассчитываем разницу позиций и переводим её в градусы
-    position_difference = end_planet_position[0] - start_planet_position[0]
-    if position_difference < 0:
-        position_difference += 360.0
+    #     start_position = swe.calc_ut(start_jd_date, planet_id)
+    #     end_position = swe.calc_ut(end_jd_date, planet_id)
+    #     planet_name = planet_names[planet_id]
 
-    # Рассчитываем скорость планеты (градусы в сутки)
-    planet_speed = position_difference / 1.0  # Перевод в градусы в сутки
+    #     return planet_name, planet_id, start_position, end_position
 
-    # Рассчитываем сколько градусов планета прошла за заданное время
-    elapsed_hours = hours - start_planet_position[0]
-    if elapsed_hours < 0:
-        elapsed_hours += 360.0
-
-    # Рассчитываем позицию планеты на заданное время
-    current_planet_position = start_planet_position[0] + (planet_speed * elapsed_hours)
-
-    return planet_id, planet_names[planet_id], current_planet_position
-
-def calculate_planet_degrees(planet_positions):
-    """
-    Вычисление градусов планет из позиций планет
-
-    :params planet_positions: Список позиций планет
-
-    :returns: Словарь с градусами планет
-    """
-    planet_degrees = {}
     
-    for planet_pos in planet_positions:
-        planet_id = planet_pos['planet_id']
-        longitude = planet_pos['longitude']
-        planet_degrees[planet_id] = longitude
-    
-    return planet_degrees
+
+
+    def get_planet_positions(self):
+        planet_names = {
+            swe.SUN: "Sun",
+            swe.MOON: "Moon",
+            swe.MERCURY: "Mercury",
+            swe.VENUS: "Venus",
+            swe.MARS: "Mars",
+            swe.JUPITER: "Jupiter",
+            swe.SATURN: "Saturn",
+            swe.URANUS: "Uranus",
+            swe.NEPTUNE: "Neptune",
+            swe.PLUTO: "Pluto",
+            swe.MEAN_NODE: "Mean Node",
+            swe.TRUE_NODE: "True Node"
+        }
+        
+        julian_day_start = swe.julday(self.year, self.month, self.day, self.hour, self.minute, 0)
+        julian_day_next = swe.julday(self.year, self.month, self.day + 1, self.hour, self.minute, 0)
+        
+        planet_positions = {}
+        
+        for planet_id in planet_names.keys():
+            planet_position_start = swe.calc_ut(julian_day_start, planet_id)[0]
+            planet_position_next = swe.calc_ut(julian_day_next, planet_id)[0]
+            position_difference = planet_position_next - planet_position_start
+            
+            if position_difference < 0:
+                retrograde = True
+            else:
+                retrograde = False
+            
+            daily_distance = abs(position_difference)
+            distance_per_second = daily_distance / (24 * 3600)
+            
+            position_at_time = planet_position_start + (self.hour * 3600 + self.minute * 60 + self.second) * distance_per_second
+            
+            if retrograde:
+                position_at_time -= (self.hour * 3600 + self.minute * 60 + self.second) * distance_per_second
+            
+            planet_positions[planet_names[planet_id]] = position_at_time
+        
+        return planet_positions
+
