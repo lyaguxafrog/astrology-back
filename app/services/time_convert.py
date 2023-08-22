c11bcec01ce9f2da1eff51d220a2d70fefb4b447
adrian <maccree.a@yandex.ru>
2023-08-23 02:46:54 +0300

    получения часового пояса по местуположению

--- app/services/time_convert.py
+++ app/services/time_convert.py
@@ -1,9 +1,35 @@
 # app/services/time_convert.py
 from datetime import datetime, timedelta
+from timezonefinder import TimezoneFinder
 import math
 
+
+def get_time_zone(latitude: float, longitude: float) -> int:
+    """
+    Получения часового пояса UTC в формате HH
+
+    :params latitude: Широта
+    :params longitude: Долгота
+
+    :returns: Час смешения
+    """
+    tf = TimezoneFinder()
+    timezone_str = tf.timezone_at(lng=longitude, lat=latitude)
+
+    if timezone_str is None:
+        return None
+
+    from datetime import datetime
+    import pytz
+
+    now = datetime.now(pytz.timezone(timezone_str))
+    utc_offset = int(now.utcoffset().total_seconds() / 3600)
+
+    return utc_offset
+
+
 def get_julian_datetime(year: int, month: int, day: int,
-                         hour: int, minute: int):
+                        hour: int, minute: int) -> float:
     """
     Конвертация даты и времени в Юлианский календарь
 
@@ -17,50 +43,43 @@ def get_julian_datetime(year: int, month: int, day: int,
 
     """
     # Создание объекта datetime для заданных параметров
-    date = datetime(year=year, month=month, day=day, 
+    date = datetime(year=year, month=month, day=day,
                     hour=hour, minute=minute, second=0)
 
     if not isinstance(date, datetime):
         raise TypeError(
             'Invalid type for parameter "date" - expecting datetime'
-            )
+        )
     elif date.year < 1801 or date.year > 2099:
         raise ValueError(
             'Datetime must be between year 1801 and 2099'
-            )
+        )
 
     # Вычисление Юлианской даты
     julian_datetime = 367 * date.year - int(
         (7 * (date.year + int(
-        (date.month + 9) / 12.0))) / 4.0) + int(
+            (date.month + 9) / 12.0))) / 4.0) + int(
         (275 * date.month) / 9.0) + date.day + 1721013.5 + (
         date.hour + date.minute / 60.0 + date.second / math.pow(
-        60, 2)) / 24.0 - 0.5 * math.copysign(
+            60, 2)) / 24.0 - 0.5 * math.copysign(
         1, 100 * date.year + date.month - 190002.5) + 0.5
 
     return julian_datetime
 
-def time_zone_convert(hour: int, zone: str) -> int:
+
+def time_zone_convert(hour: int, zone: int) -> int:
     """
     Конвертация часовой зоны в UTC-00:00
 
     :param hour: Час для конвертации
-    :param zone: Часовой пояс в формате UTC±HH:MM
+    :param zone: Часовой пояс в формате H
 
     :returns: Час в UTC-00:00 (в виде целого числа)
     """
-    if zone == 'UTC±00:00':
-        return int(hour)  # Если уже в UTC-00:00, возвращаем исходное значение часа
+    if zone == 0:
+        # Если уже в UTC-00:00, возвращаем исходное значение часа
+        return int(hour)
     else:
-        zone_hours = int(zone[4:6])  # Получаем часовой сдвиг
-        zone_minutes = int(zone[7:9])  # Получаем минутный сдвиг
+        new_time = hour + zone
         
-        input_time = datetime.strptime(str(hour), '%H')  # Создание объекта datetime
-        
-        time_difference = timedelta(hours=zone_hours, minutes=zone_minutes)  # Рассчет разницы времени
-        
-        utc_time = input_time + time_difference  # Прибавление разницы для перевода в UTC
-        
-        utc_hour = utc_time.strftime('%-H')  # Преобразование в строку без нуля в начале
-        
-        return int(utc_hour)  # Возвращаем UTC-час как целое число
+        return new_time
